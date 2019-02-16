9f664f7515880cea127eb90f301bc95c64ccfb39
Jacob Waites <jwaites@ideo.com>
2019-02-15 21:13:13 -0500

    intial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+/**
+* Created with IDEO CoLab Domain Template.
+* User: jacobwaites
+* Date: 2016-11-07
+* Time: 05:54 PM
+* To change this template use Tools | Templates.
+*/
+ $(document).ready(function(){
+	$('.navtrig').click(function () {
+		$('.scrolly-nav').toggleClass('open');
+	});
+     
+     $('.list-link').click(function () {
+		$('.scrolly-nav').toggleClass('open');
+	});
+ });
+     
